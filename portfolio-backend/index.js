c6ea4446029ec7fe07b0b03d08cc11e4d6201ec5
iddyimasha-sys <iddyimasha@gmail.com>
2026-06-02 16:09:47 +0300

    portfolio-fronted

---
+++ portfolio-backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/api/profile', (req, res) => {
+    res.json({
+        name: "Sharon Saidi Sogoi",
+        role: "Data Scientist"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running smoothly on port ${PORT}`);
+});
